6cbd9dd5dee7fb4d80f03c8ed80ece2bb86481b9
SAKTHIMURUGAN5610 <sakthimuruganrajavel10@gmail.com>
2025-08-08 15:03:22 +0530

    Update text.py

--- text.py
+++ text.py
@@ -6,7 +6,7 @@ st.header("MLB Game prediction")
 # team=st.text_input("enter your prompt")
 # st.write(
 # f"your winning : {team}")
-st.subheader("Aug 05-Best Bet Recommendation")
+st.subheader("Aug 12-Best Bet Recommendation")
 body = """
 
 Miami Marlins (+1.5 at -185)   - perplexity
@@ -18,4 +18,5 @@ st.code(body)
 st.subheader("AI Prediction Data")
 re=pd.read_csv("mlb_betting_analysis.csv")
 st.write(re)
+
 
